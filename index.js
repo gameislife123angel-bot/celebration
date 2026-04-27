97e3a88586680aa612c111d1aeaed615b15c0bb6
Angel <gameislife123angel@gmail.com>
2026-04-27 23:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cake = document.getElementById("cake");
 const gift = document.getElementById("gift");
 const blessings = document.getElementById("blessings");
 const title = document.getElementById("title");
+const credit = document.getElementById("credit");
 
 let cakeDone = false;
 
@@ -10,13 +11,16 @@ cake.onclick = () => {
     if (cakeDone) return;
     cakeDone = true;
 
+    // Fade credit
+    credit.classList.add("fade");
+
     // Blow candle
     document.querySelector(".flame").style.display = "none";
 
     // Cut cake
     cake.classList.add("cut");
 
-    // Change title
+    // Update title
     setTimeout(() => {
         title.innerText = "🎉 Congratulations Dad! Chief Manager (CM) 🎉";
     }, 500);
